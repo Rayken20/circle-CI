3a777213efe20d90820acccc8debffeb34f7310f
Rachael <rachael92njoki@gmail.com>
2024-01-07 08:52:05 +0300

    circle

---
+++ circle.js
@@ -0,0 +1,10 @@
+function calculateSquare(number) {
+    return number * number;
+  }
+  
+  // Example usage of the function
+  var inputNumber = 5;
+  var result = calculateSquare(inputNumber);
+  
+  // Display the result in the console
+  console.log("The square of " + inputNumber + " is: " + result);
